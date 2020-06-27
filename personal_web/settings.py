779f0b529ede0e4b6bf3051424e02bed8a946c5b
Mayank Jha <mayank@Mayanks-MacBook-Pro-2.local>
2020-06-27 00:10:13 -0400

    project grid

--- personal_web/settings.py
+++ personal_web/settings.py
@@ -28,7 +28,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
 ALLOWED_HOSTS = ['mayankj.herokuapp.com']
 
