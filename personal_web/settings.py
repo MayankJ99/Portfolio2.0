aec690607d2849f7195da2d94e297579601530b7
Mayank Jha <mayank@Mayanks-MacBook-Pro-3.local>
2020-10-09 06:59:43 +0530

    removed aos for fast load, reduced card size

--- personal_web/settings.py
+++ personal_web/settings.py
@@ -23,6 +23,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "e75551e924c9ee96f1ed1c12775a90c4cf79d3655ad6f72f"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
