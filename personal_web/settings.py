010349ccecfe6f5d1cb4b7bae9ae092b83878bbe
MayankJ99 <mayanksjha@gmail.com>
2020-11-04 19:57:59 +0530

    resume field

--- personal_web/settings.py
+++ personal_web/settings.py
@@ -140,7 +140,6 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
