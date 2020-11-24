088dafe4c07d4c50514f20ccad140f990a86ec93
MayankJ99 <mayanksjha@gmail.com>
2020-11-24 20:25:32 +0530

    debug false

--- personal/views.py
+++ personal/views.py
@@ -17,7 +17,7 @@ def handler404(request, *args, **argv):
 
 
 def handler500(request, *args, **argv):
-    return render(request, '404.html', status=404)
+    return render(request, '500.html', status=404)
 
 
 def blogList(request):
